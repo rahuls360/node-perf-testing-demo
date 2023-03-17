a7dabae3b05fef944b55cced558683e6a9b9a43b
Rahul Makhija <rahuls360@gmail.com>
2023-03-17 22:27:44 +0530

    add base setup for node express server - typescript, es modules

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello!')
+});
+
+app.listen(PORT, () => {
+  console.log("Server is listening on PORT: ", PORT);
+  
+})
