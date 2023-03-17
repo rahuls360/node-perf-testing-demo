da386c27893c84133400b98adfeff0b7f934209f
Rahul Makhija <rahuls360@gmail.com>
2023-03-17 23:45:26 +0530

    migrate to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+import NodemonPlugin from 'nodemon-webpack-plugin';
+import nodeExternals from 'webpack-node-externals';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: './src/server.ts',
   devtool: 'inline-source-map',
   module: {
@@ -15,8 +23,20 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new NodePolyfillPlugin(),
+    new NodemonPlugin(),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  target: 'node', // ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // ignore all modules in node_modules folder
 };
